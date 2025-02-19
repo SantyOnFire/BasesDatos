5ad7f7b79a89c92bc0665d6a16812708a00b9935
Santiago <smalpica182@gmail.com>
2025-02-19 11:29:44 -0500

    Actualizando mensaje en Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,8 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-        public static void main(String[] args) {
-            //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-            // to see how IntelliJ IDEA suggests fixing it.
-            System.out.printf("Hello and welcome!");
+    public static void main(String[] args) {
+        System.out.println("Estamos aprendiendo comandos Git y estamos viendo comandos");
 
-
-
-        }
+    }
 }
